70ff569ce848192d3ce397a080c22461f2740e15
k18rs063 <k18rs063@st.kyusan-u.ac.jp>
2019-12-02 14:33:26 +0900

    スコア保存

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,32 +1,13 @@
 // This is a JavaScript file
 var ncmbController = {
-   APPLICATION_KEY: "432b2292f9ecb00a01a3e04c2b478d7c673b8ead0784077cd4e24298cde13a41",
-   CLIENT_KEY: "ea81164a2a79dd91a1de7ee86d33bf37fe4905fe7c6d73759651fe029b5ecf94",
+   APPLICATION_KEY: appKey,
+   CLIENT_KEY: clientKey,
 
     ncmb: null,
     currentUser: null,  // ログインしたユーザーのオブジェクトを格納
     screenSize: null,    // 画面サイズを格納
 
     sendScore: function(score) {
-    var self = this;
-
-    // [1]Score（クラス）を生成
-    var Score = self.ncmb.DataStore("ScoreClass");
-
-    // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
-    var scoreData = new Score({score: score});
-
-    // [3]送信処理
-    scoreData.save()
-        .then(function (saved) {
-            alert("スコア送信完了！");
-        })
-       .catch(function(err){
-            console.log(err);
-        });
-},
-
-sendScore: function(score) {
     var self = this;
 
     // [1]Score（クラス）を生成
@@ -59,6 +40,7 @@ sendScore: function(score) {
         }
     })
 },
+
 
     // 初期化
     init: function(screenSize) {
---
+++ www/keys.js
@@ -0,0 +1,7 @@
+// This is a JavaScript file
+var appKey = "432b2292f9ecb00a01a3e04c2b478d7c673b8ead0784077cd4e24298cde13a41";
+
+var clientKey = "ea81164a2a79dd91a1de7ee86d33bf37fe4905fe7c6d73759651fe029b5ecf94";
+
+
+
